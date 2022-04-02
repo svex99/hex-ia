d81317166ab9867db433d8b2d504b22164c8efba
NazDia <w.damian.ohallorans.42@gmail.com>
2022-04-02 03:05:39 +0200

    heuristica funcionando contra rush 100%

--- minimax_player.py
+++ minimax_player.py
@@ -45,7 +45,42 @@ def moves(game, player):
 				yield (x, y)
 
 
-def heuristic(game, player):
-	return 0
+def valued_bfs(game: Game, beginning, end, player, pos_stack, current_val=0):
+	if len(beginning) == 0:
+		return current_val
+
+	current = 0
+	next_l = []
+	for i in beginning:
+		nextpos_gen = list(game.neighbour(i[0], i[1]))
+		for nextpos in nextpos_gen:
+			if nextpos in pos_stack:
+				pass
+			elif game[nextpos[0], nextpos[1]] == player:
+				val = current
+				beginning.append(nextpos)
+				pos_stack.append(nextpos)
+			elif game[nextpos[0], nextpos[1]] == EMPTY:
+				val = 1 + current
+				next_l.append(nextpos)
+				pos_stack.append(nextpos)
+			else:
+				pass
+
+	return valued_bfs(game, next_l, end, player, pos_stack, current_val + 1)
+
+def heuristic(game : Game, player):
+	size = len(game.board)
+	pre_b_pos = [(0, i) for i in range(size)]
+	post_b_pos = [(size -1, i) for i in range(size)]
+	pre_w_pos = [(i, 0) for i in range(size)]
+	post_w_pos = [(i, size -1) for i in range(size)]
+	beginning = pre_w_pos if player == PLAYER[0] else pre_b_pos
+	end = post_w_pos if player == PLAYER[0] else post_b_pos
+	ret = valued_bfs(game, beginning, end, player, [])
+	beginning = pre_w_pos if player == PLAYER[1] else pre_b_pos
+	end = post_w_pos if player == PLAYER[1] else post_b_pos
+	ret -= valued_bfs(game, beginning, end, player, [])
+	return -ret
 
 
